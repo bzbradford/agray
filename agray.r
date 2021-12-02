43787ccd23b3bf6975ee73af03da1bdbb53ed065
Ben Bradford <bradfordium@gmail.com>
2021-12-01 23:49:43 -0600

    improved error handling and messaging

--- agray.r
+++ agray.r
@@ -5,53 +5,71 @@
 
 library(tidyverse)
 
-agray <- function(file, alias) {
-  
+agray <- function(file) {
   require(tidyverse)
   
-  message("Loading file '", file, "' with alias '", alias, "'...", "\n")
+  alias <- tools::file_path_sans_ext(basename(file))
+  out_dir <- file.path(dirname(file), alias)
   
-  dir.create(alias, showWarnings = F)
-  
-  # read raw csv
-  df <- suppressMessages(read_csv(file))
+  # read csv
+  message("Loading grader file '", file, "' and saving outputs to '", out_dir, "'")
+  dir.create(out_dir, showWarnings = F)
+  df <- suppressWarnings(read_csv(file, col_types = cols(.default = "c")))
   
   # clean up raw csv and remove culls
+  message("\nCleaning input file...")
   df_clean <- df %>%
-    filter(!is.na(Researcher),
+    filter(
+      !is.na(Researcher),
       Researcher != "Researcher",
       Researcher != "Culls Weight") %>%
-    type_convert() %>%
-    mutate(Size = pmap_dbl(list(Width, Length, Height), ~ sort(c(..1, ..2, ..3))[2])) %>%
-    mutate(Grade = case_when(
-      Size >= 1.875 ~ "A",
-      Size >= 1.5 ~ "B",
-      T ~ "C"))
-  
-  # get ordered plot list
-  plots <- unique(df_clean$Plot)
-  message("\nPlots:")
-  print(plots)
-  
-  if (is.character(df_clean$Plot)) { message("Warning: Non-numeric plot number detected.") }  
+    type_convert(col_types = cols()) %>%
+    mutate(
+      Size = pmap_dbl(list(Width, Length, Height), ~ sort(c(..1, ..2, ..3))[2]),
+      Grade = case_when(
+        Size >= 1.875 ~ "A",
+        Size >= 1.5 ~ "B",
+        T ~ "C"))
   
   # save tuber list
-  tuber_path <- file.path(alias, paste(alias, "tubers.csv"))
+  tuber_path <- file.path(out_dir, paste(alias, "- graded tuber list.csv"))
   df_clean %>% write_csv(tuber_path)
-  message("\nSaved graded tuber list to '", tuber_path, "'")
+  cat("Saving graded tuber list to:", tuber_path, "\n")
+  
+  # get ordered plot list
+  message("\nGetting plots...")
+  plots <- unique(df_clean$Plot)
+  if (anyNA(plots)) {
+    stop("Missing value(s) in plot name column, check data!")
+  }
+  cat("As run:", paste(plots, collapse = ", "), "\n")
+  cat("Sorted:", paste(sort(plots), collapse = ", "), "\n")
+  cat("Total plots:", length(plots), "\n")
+  if (is.character(df_clean$Plot)) {
+    message("Non-numeric plot number detected, confirm plot names!")
+  }
   
   # get culls
+  message("\nGetting cull weights...")
   culls <- df %>%
     filter(Researcher == "Culls Weight") %>%
     select(Trial) %>%
-    rename(cull_wt = Trial) %>%
-    cbind(tibble(Plot = plots), .)
+    rename(cull_wt = Trial)
   
-  culls_path <- file.path(alias, paste(alias, "culls.csv"))
-  culls %>% write_csv(culls_path)
-  message("\nSaved culls list to '", culls_path, "'")
+  # check for culls error
+  if (length(plots) != nrow(culls)) {
+    culls <- tibble(Plot = plots, cull_wt = NA)
+    cat("ERROR: Failed to parse cull weights, check for extra cull weights or unnamed plots in data!\n")
+    warning("Failed to parse cull weights, check for extra cull weights or unnamed plots in data!")
+  } else {
+    culls <- cbind(tibble(Plot = plots), culls)
+    culls_file <- file.path(out_dir, paste(alias, "- cull weights.csv"))
+    cat("Saving cull weights to:", culls_file, "\n")
+    write_csv(culls, culls_file)
+  }
   
   # total summary
+  message("\nSummarizing dataset...")
   summary1 <- df_clean %>%
     group_by(Plot) %>%
     summarise(
@@ -101,18 +119,25 @@ agray <- function(file, alias) {
       starts_with("prp_"),
       everything())
   
-  summary_path <- file.path(alias, paste(alias, "grading summary.csv"))
+  summary_path <- file.path(out_dir, paste(alias, "- plot summaries.csv"))
   summary %>% write_csv(summary_path)
-  message("\nGrading summary saved to '", summary_path, "'")
+  cat("Saving plot-wise grading summaries to:", summary_path, "\n")
   
   
   # simple plot
+  message("\nGenerating summary plot...")
+  if (is.numeric(summary$Plot)) {
+    summary <- summary %>%
+      arrange(Plot) %>%
+      mutate(Plot = fct_inorder(as.character(Plot)))
+  }
+  
   plot <- summary %>%
-    ggplot(aes(x = as.character(Plot), y = total_wt)) +
-    geom_col(aes(fill = total_wt)) +
+    ggplot(aes(x = Plot, y = total_wt)) +
+    geom_col(aes(fill = total_wt), color = "black", size = 0.25) +
     scale_y_continuous(expand = expansion(c(0, 0.1))) +
     labs(
-      title = "Total plot weights",
+      title = paste(alias, "- Total plot weights"),
       x = "Plot",
       y = "Total weight (oz)",
     ) +
@@ -122,11 +147,9 @@ agray <- function(file, alias) {
       axis.text.x = element_text(angle = -90, hjust = 0, vjust = 0.5))
   show(plot)
   
-  plot_path <- file.path(alias, paste(alias, "total weights plot.png"))
-  ggsave(plot_path, plot, type = "cairo")
+  plot_path <- file.path(out_dir, paste(alias, "- total weights plot.png"))
+  cat("Saving plot image to:", plot_path, "\n")
+  suppressMessages(ggsave(plot_path, plot, type = "cairo"))
+  
+  message("\nDone!")
 }
-
-
-# Run example data
-# arguments are [file], [output file name prefix]
-agray("example-data.csv", "example")
---
+++ example.r
@@ -0,0 +1,5 @@
+# Load the function definition script
+source("agray.r")
+
+# Run example data
+agray("Example data.csv")
